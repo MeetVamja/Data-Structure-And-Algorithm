5a6165e9ac9d3eb6e1dc0e6cd051d05a5af8cc7e
Meet Vamja <98141044+MeetVamja@users.noreply.github.com>
2026-02-02 00:28:42 +0530

    Create binary_exponential.cpp

---
+++ Maths/binary_exponential.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// notes: Follow blog of binary exponential from cp-algorithms
+// recursive version
+long long binpow(long long a, long long b)
+{
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res;
+}
+
+// actual implementation using binary exponential
+long long binpow(long long a, long long b)
+{
+    long long res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+// Effective computation of large exponents modulo a number
+// simply add modulo in every multiplications
+long long binpow(long long a, long long b, long long m)
+{
+    a %= m;
+    long long res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+int main()
+{
+    // here we are talking about a^b
+}
