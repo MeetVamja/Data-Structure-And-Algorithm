05e54d172890abdb0bab08244f6b4e3497d04abd
Meet Vamja <98141044+MeetVamja@users.noreply.github.com>
2026-07-08 00:47:27 +0530

    Convert Kahn's Algorithm C++
    Converted JavaScript implementation of Kahn's Algorithm for cycle detection in directed graphs to C++.

--- Graphs/cycle detection in directed graph using bfs .cpp
+++ Graphs/cycle detection in directed graph using bfs .cpp
@@ -1,32 +1,32 @@
-// cycle detection in directed graph using bfs 
-// Kahn's Algorithm
-
-function topoSort(adj) {
-    n = adj.length
-    let indegree = new Array(n).fill(0);
-    let queue = [];
-    let res = [];
+vector<int> topoSort(vector<vector<int>>& adj) {
+    int n = adj.size();
+    vector<int> indegree(n, 0);
+    queue<int> q;
+    vector<int> list;
 
     // Compute indegrees
-    for (let i = 0; i < n; i++) {
-        for (let next of adj[i]) indegree[next]++;
+    for (int i = 0; i < n; i++) {
+        for (int next : adj[i])
+            indegree[next]++;
     }
 
     // Add all nodes with indegree 0 
     // into the queue
-    for (let i = 0; i < n; i++) {
-        if (indegree[i] === 0) queue.push(i);
-    }
+    for (int i = 0; i < n; i++)
+        if (indegree[i] == 0)
+            q.push(i);
 
-    // Kahn’s Algorithm
-    while (queue.length > 0) {
-        let top = queue.shift();
-        res.push(top);
-        for (let next of adj[top]) {
+    // Kahn’s Algorithm (BFS)
+    while (!q.empty()) {
+        int top = q.front();
+        q.pop();
+        list.push_back(top);
+        for (int next : adj[top]) {
             indegree[next]--;
-            if (indegree[next] === 0) queue.push(next);
+            if (indegree[next] == 0)
+                q.push(next);
         }
     }
 
-    return res;
+    return list;
 }
